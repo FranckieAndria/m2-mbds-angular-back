80e9d9c0a0fbd193f11c9a627637752926af68f5
FranckieAndria <80829683+FranckieAndria@users.noreply.github.com>
2024-03-21 07:46:08 +0300

    Final Schema Mongoose

---
+++ models/Assignment.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema ;
+const ObjectId = require("mongodb").ObjectId ;
+
+const AssignmentSchema = new Schema({
+    titre: { type: String, required: true },
+    etudiant: { type: ObjectId, ref: "Etudiant", required: true },
+    professeur: { type: ObjectId, ref: "Professeur", required: true },
+    dateDeCreation: { type: Date, required: true, default: Date.now },
+    dateDeRendue: { type: Date, required: false },
+    note: { type: Number, required: true, default: -1 },
+    remarque: { type: string, required: false, default: '' },
+    rendu: { type: Boolean, required: true, default: false }
+})
+
+module.exports = mongoose.model("Assignment", AssignmentSchema) ;
--- models/Etudiant.js
+++ models/Etudiant.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema ;
-const ObjectId = require("mongodb").ObjectId ;
 
 const EtudiantSchema = new Schema({
     nom: { type: String, required: true },
@@ -9,16 +8,7 @@ const EtudiantSchema = new Schema({
     password: { type: String, required: true },
     role: { type: String, required: true },
     niveau: { type: String, required: true },
-    imagePath: { type: String, required: true },
-    assignments: [{
-        titre: { type: String, required: true },
-        professeur: { type: ObjectId, ref: "Professeur", required: true } ,
-        dateDeCreation: { type: Date, required: true, default: Date.now } ,
-        dateDeRendure: { type: Date, required: false } ,
-        note: { type: Number, required: true, default: -1 } ,
-        remarque: { type: string, required: false, default: '' } ,
-        rendu: { type: Boolean, required: true, default: false }
-    }]
+    imagePath: { type: String, required: true }
 }) ;
 
 module.exports = mongoose.model("Etudiant", EtudiantSchema) ;
