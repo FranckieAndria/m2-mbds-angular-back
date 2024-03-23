d41ddec3cfbffcf1fda7e8e3c59c934dc1d4c70d
FranckieAndria <franckieandriamalala@gmail.com>
2024-03-23 13:36:12 +0300

    Liste assignments - Pagination - Sort - Filter

--- services/etudiantService.js
+++ services/etudiantService.js
@@ -4,22 +4,30 @@ const Etudiant = require('../models/Etudiant') ;
 const { ObjectId } = require('mongodb');
 const { loginUser } = require('./userService') ;
 
+
+/*************************************
+* CONSTANTS FOR ALL FUNCTIONS - START *
+*************************************/
+const lookUpProfesseur = {
+    from: 'professeurs',
+    localField: 'professeur',
+    foreignField: '_id',
+    as: 'professeur',
+    pipeline: [{$project: {nom: 1, prenom: 1, email: 1, matiere: 1, imagePath: 1}}]
+} ;
+/***********************************
+* CONSTANTS FOR ALL FUNCTIONS - END *
+***********************************/
+
+
 /* LISTE des ASSIGNMENTS d'un ETUDIANT [PAGINATION - SORT - (Filtre rendu | non-rendu)] */
 const listeAssignment = async (req, res) => {
     const aggregateQuery = Assignment.aggregate();
     const renduQuery = parseInt(req.query.rendu) || 'all' ;
     const matching = renduQuery == 'all' ? {etudiant: ObjectId(req.params.id)} : {etudiant: ObjectId(req.params.id),rendu: renduQuery == 1} ;
     aggregateQuery.match(matching);
-    aggregateQuery.sort({
-        dateDeRendu: parseInt(req.query.tri) || 1
-    });
-    aggregateQuery.lookup ({
-        from: 'professeurs',
-        localField: 'professeur',
-        foreignField: '_id',
-        as: 'professeur',
-        pipeline: [{$project: {nom: 1, prenom: 1, email: 1, matiere: 1, imagePath: 1}}]
-    });
+    aggregateQuery.sort({dateDeRendu: parseInt(req.query.tri) || 1});
+    aggregateQuery.lookup (lookUpProfesseur);
 
     Assignment.aggregatePaginate(
         aggregateQuery,
@@ -34,8 +42,11 @@ const listeAssignment = async (req, res) => {
     );
 } ;
 
-
 /* RECHERCHE par [ETUDIANT connecté] - MATIERE - DATE de CRÉATION - DATE de RENDU [PAGINATION] */
+const searchAssignment = async (req, res) => {
+
+} ;
+
 
 /* AJOUT D'UN NOUVEL ASSIGNMENT */
 
@@ -66,5 +77,6 @@ const login = async (req, res) => {
 
 module.exports = {
     login,
-    listeAssignment
+    listeAssignment, 
+    searchAssignment
 } ;
