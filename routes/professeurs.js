11abca1b2a12c4eff4a01dac1824b19280d22ef3
FranckieAndria <franckieandriamalala@gmail.com>
2024-03-24 13:27:25 +0300

    Liste des assignments d'un professeur

--- routes/professeurs.js
+++ routes/professeurs.js
@@ -1,7 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
-const { login } = require('../services/professeurService');
+const { login, listeMatiere, listeAssignment } = require('../services/professeurService');
+
+/* LISTE des MATIÈRES pour l'enregistrement d'un NOUVELL ASSIGNMENT */
+router.get('/matieres', listeMatiere);
+
+/* LISTE des ASSIGNMENTS d'un PROFESSEUR [PAGINATION - par CATÉGORIE RENDU ou NON-RENDU] */
+router.get('/:id/assignments', listeAssignment);
 
 /* Login */
 router.post('/login', login) ;
--- services/professeurService.js
+++ services/professeurService.js
@@ -1,14 +1,63 @@
+const Assignment = require('../models/Assignment');
 const Professeur = require('../models/Professeur') ;
+
+const { ObjectId } = require('mongodb');
 const { loginUser } = require('./userService');
 
-/* LISTE des ASSIGNMENTS d'un PROFESSEUR [PAGINATION - par CATÉGORIE RENDU ou NON-RENDU] */
+/*************************************
+* CONSTANT FOR ALL FUNCTIONS - START *
+*************************************/
+const MIN_DATE = "2000-01-01";
+const MAX_DATE = "2100-12-31";
+const DEFAULT_SORT = -1 ;
+
+function getLookupEtudiant(pipeline) {
+    return {
+        from: 'etudiants',
+        localField: 'etudiant',
+        foreignField: '_id',
+        as: 'etudiant',
+        pipeline: pipeline
+    };
+}
+
+function sendPaginatedResult(aggregateQuery, res, page, limit) {
+    Assignment.aggregatePaginate(
+        aggregateQuery,
+        {
+            page: parseInt(page) || 1,
+            limit: parseInt(limit) || 10
+        },
+        (err, data) => {
+            if (err) res.send(err);
+            res.send(data);
+        }
+    );
+}
+/***********************************
+* CONSTANT FOR ALL FUNCTIONS - END *
+***********************************/
+
+/* LISTE des ASSIGNMENTS d'un PROFESSEUR [PAGINATION - SORT - par CATÉGORIE RENDU ou NON-RENDU] */
+const listeAssignment = async (req, res) => {
+    const aggregateQuery = Assignment.aggregate();
+    const renduQuery = parseInt(req.query.rendu) || 'all';
+    const matching = renduQuery == 'all' ? { professeur: ObjectId(req.params.id) } : { professeur: ObjectId(req.params.id), rendu: renduQuery == 1 };
+    aggregateQuery.match(matching);
+    aggregateQuery.sort({ dateDeRendu: parseInt(req.query.tri) || DEFAULT_SORT });
+    aggregateQuery.lookup(getLookupEtudiant([{ $project: { nom: 1, prenom: 1, email: 1, imagePath: 1 } }]));
+    sendPaginatedResult(aggregateQuery, res, req.query.page, req.query.limit);
+};
 
 /* RECHERCHE par [PROFESSEUR connecté] - MATIERE - DATE de CRÉATION - DATE de RENDU [PAGINATION] */
 
 /* METTRE UN ASSIGNMENT en RENDU (Envoyé un email à l'ETUDIANT) */
 
 /* LISTE des MATIÈRES pour l'enregistrement d'un NOUVELL ASSIGNMENT */
-
+const listeMatiere = async (req, res) => {
+    const professeurs = await Professeur.find().select('nom prenom matiere.intitule').exec();
+    res.send(professeurs);
+};
 
 // LOGIN - START
 const login = async (req, res) => {
@@ -32,5 +81,7 @@ const login = async (req, res) => {
 // LOGIN - END
 
 module.exports = {
-    login
+    login, 
+    listeMatiere,
+    listeAssignment
 } ;
