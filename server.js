42ec157b5eb6a0c2c58d3a07522ee68922b5ce56
FranckieAndria <franckieandriamalala@gmail.com>
2024-03-27 22:47:26 +0300

    Cors - Verify token

--- server.js
+++ server.js
@@ -1,25 +1,47 @@
+/***********************
+* REQUIREMENTS - START *
+***********************/
 let express = require('express');
+const cors = require('cors');
 let app = express();
 let bodyParser = require('body-parser');
+/*********************
+* REQUIREMENTS - END *
+*********************/
 
-// Connexion à la base de données sur Atlas
+/***********************************************
+* CONNEXION À LA BASE de DONNÉES ATLAS - START *
+***********************************************/
 require ('./services/atlas-connexion.js') ;
+/*********************************************
+* CONNEXION À LA BASE de DONNÉES ATLAS - END *
+*********************************************/
 
-// Pour accepter les connexions cross-domain (CORS)
+/*******************************************
+* CONNEXIONS (CROSS-DOMAIN | CORS) - START *
+*******************************************/
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   next();
 });
+/*******************************************
+* CONNEXIONS (CROSS-DOMAIN | CORS) - END *
+*******************************************/
 
-// Pour les formulaires
+/*********************
+* MIDDLEWARE - START *
+*********************/
+app.use(cors()) ;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+/*******************
+* MIDDLEWARE - END *
+*******************/
 
 // Obligatoire si déploiement dans le cloud !
 let port = process.env.PORT || 8010;
-
 
 /*****************
 * ROUTES - START *
@@ -31,7 +53,6 @@ app.use(prefix + '/assignments', require('./routes/assignments'));
 /*****************
 * ROUTES - START *
 *****************/
-
 
 // On démarre le serveur
 app.listen(port, "0.0.0.0");
