fc22229650f9faa33a4d1d99aa80729e84ca1f37
Franckie ANDRIAMALALA <80829683+FranckieAndria@users.noreply.github.com>
2024-04-01 11:32:53 +0300

    Update error code

--- services/tokenValidator.js
+++ services/tokenValidator.js
@@ -5,7 +5,7 @@ function verifyToken(req, res, next) {
     var token = req.headers['x-access-token'];
     if (!token) return res.status(403).send({ auth: false, message: 'Erreur d\'authentication' });
     jwt.verify(token, SECRET_KEY, (err, decoded) => {
-        if (err) return res.status(500).send({ auth: false, message: 'Erreur de la validation' });
+        if (err) return res.status(403).send({ auth: false, message: 'Erreur de la validation' });
         req.params.id = decoded.user._id ;
         next();
     });
