d20eb1e28801b23a39d9413d8ea23134f04d7d54
Franckie ANDRIAMALALA <80829683+FranckieAndria@users.noreply.github.com>
2024-04-15 21:24:30 +0300

    Update contenu mail

--- services/assignmentService.js
+++ services/assignmentService.js
@@ -46,10 +46,10 @@ const details = async (req, res) => {
 /* MODIFICATION D'UN ASSIGNMENT (Si l'assignment est rendu => Envoyé un email à l'ETUDIANT) */
 const updateAssignment = async (req, res) => {
     const result = await Assignment.findByIdAndUpdate(req.params.id, req.body, { new: true });
-    if (req.body.rendu == 'true') {
+    if (result != null && req.body.rendu == true) {
         const assign = await Assignment.findById(ObjectId(req.params.id)).populate('etudiant', 'email').populate('professeur', 'nom prenom matiere.intitule').exec();
         const professeur = assign.professeur.nom + ' ' + assign.professeur.prenom;
-        sendMail(assign.etudiant.email, getEmailContent(req.body.titre, professeur, assign.professeur.matiere.intitule, parseInt(req.body.note), req.body.remarque));
+        sendMail(assign.etudiant.email, getEmailContent(assign.titre, professeur, assign.professeur.matiere.intitule, parseInt(req.body.note), req.body.remarque));
     }
     res.send({
         updated: result != null,
@@ -63,7 +63,7 @@ function getEmailContent(titre, professeur, matiere, note, remarque) {
             <li><b>Titre : </b>` + titre + `</li>
             <li><b>Professeur : </b>` + professeur + `</li>
             <li><b>Matière : </b>` + matiere + `</li>
-            <li><b>Note : </b>` + note + `</li>
+            <li><b>Note : </b>` + note + `/20</li>
             <li><b>Remarque : </b>` + remarque + `</li><br>Cordialement,` ;
     return result ;
 }
